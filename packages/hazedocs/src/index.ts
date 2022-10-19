f51f7ad0b48ab7f8c4588fa33d1d708eb8f0871b
aomini daiki <rakesh.shrestha@hazesoft.co>
2022-10-19 13:29:57 +0545

    fix(hazedocs): docs generation and md files with css

--- packages/hazedocs/src/index.ts
+++ packages/hazedocs/src/index.ts
@@ -29,9 +29,39 @@ const parseToHtml = async () => {
       .process(extractedMarkdown);
     const HtmlData = String(parsedObject);
 
+    const navbarComponent: string = await fs.readFile(
+      path.join(process.cwd() + "/src/components/navbar.html"),
+      "utf-8"
+    );
+
     await fs.writeFile(
       `${reportPath}/${currentFile.replace("md", "html")}`,
-      HtmlData,
+      ` 
+        <html>
+          <head>
+            <title>${currentFile.replace(".md", " | Ecommerce Docs")}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,400;0,700;1,100&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+          </head>
+          <body>
+            <main>
+              ${navbarComponent}
+              <div class="container">
+                ${HtmlData}
+              </div>
+            </main>
+            <script>
+              /* Add table classes */
+              const tables = document.querySelectorAll("table");
+              tables.forEach((table) => {
+                table.classList.add("table");
+              });
+            </script>
+          </body>
+        </html>
+      `,
       "utf8"
     );
   }
