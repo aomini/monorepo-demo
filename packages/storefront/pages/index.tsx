7408636961b657614b2fa48a878d01782572a55b
aomini daiki <rakesh.shrestha@hazesoft.co>
2022-11-14 13:38:27 +0545

    refactor(storefront): import order

--- packages/storefront/pages/index.tsx
+++ packages/storefront/pages/index.tsx
@@ -1,6 +1,9 @@
-import type { NextPage } from 'next';
+import React from 'react';
+
+import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+
 import { sum } from '@mono/utils';
 
 import styles from '../styles/Home.module.css';
